560a51ea17bb008d8f57097f0bb5ce1978abd84d
Latawiec <lmarzec08@gmail.com>
2023-10-22 13:24:44 +0200

    Cleaning up frontend

--- _delete_savage_playground_frontend/src/components/GameCanvas/Rendering/Common/Shapes/ShapeData.ts
+++
@@ -1,7 +0,0 @@
-
-export interface ShapeData {
-    vertices: Float32Array;
-    uvs: Float32Array;
-    indices: Uint16Array;
-    elementsCount: number;
-}
---
+++ savage_playground_frontend/src/components/Game/Game.vue
@@ -0,0 +1,19 @@
+<template>
+    <GameOverlay/>
+    <GameCanvas/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import GameCanvas from './GameCanvas.vue'
+import GameOverlay from './GameOverlay.vue'
+
+
+export default defineComponent({
+    name: "Game",
+    components: {
+        GameCanvas,
+        GameOverlay,
+    }
+})
+</script>
